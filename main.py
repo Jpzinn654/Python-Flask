8a3b5a92c3f4339a0ee5ec833cc01d5a5922eeaf
Juan Pablo <juan654.pablo@gmail.com>
2024-07-23 22:36:46 -0300

    Refatoração

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+from routes.home import home_rout
 
 #Inicialização
 app = Flask(__name__)
 
-#rotas
-@app.route('/')
-def ola_mundo():
-    titulo = 'Página Inicial'
-    usuario = [
-        {'nome': 'Guilherme', 'membro_ativo': True},
-        {'nome': 'Joao', 'membro_ativo': False},
-        {'nome': 'Maria', 'membro_ativo': False},
-    ]
-    return render_template('index.html', titulo=titulo, usuario=usuario)
+@app.register_blueprint(home_rout)
 
 # Indicar que estamos no modo desenvolvedor para n encerar a aplicação a cada alteração
 app.run(debug=True)
-
